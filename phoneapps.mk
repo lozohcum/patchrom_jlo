1fb0e43ee354ef1e59b93830b27b1a4bf54a6f6c
lozohcum <lozohcum@gmail.com>
2013-08-26 15:39:00 +0200

    my first commit

---
+++ phoneapps.mk
@@ -0,0 +1,82 @@
+local-phone-apps = $(private-phone-apps)
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  BadgeProvider \
+					  BluetoothAvrcp \
+					  BluetoothMap \
+					  BluetoothTest \
+					  CSC \
+					  ClipboardSaveService \
+					  CertInstaller \
+					  DefaultContainerService \
+					  Divx \
+					  EdmVpnServices \
+					  FaceLock \
+					  FmRadio \
+					  KeyChain \
+					  LogsProvider \
+					  MediaUploader \
+					  MusicFX \
+					  MtpApplication \
+					  OmaCP \
+					  PCWClientS \
+					  PRUI \
+					  PhoneErrService \
+					  PopupuiReceiver \
+					  Preconfig \
+					  PreloadInstaller \
+					  SamsungTTS \
+					  SamsungIME \
+					  SecMediaProvider \
+					  SecurityProvider \
+					  SecLiveWallpapersPicker \
+					  Stk \
+					  UserDictionaryProvider \
+					  VpnDialogs \
+					  WAPPushManager \
+					  WlanTest \
+					  minimode-res \
+					  wssyncmlnps
+
+private-lockscreen-apps := PickUpTutorial \
+						   AccuweatherDaemon \
+						   AccuweatherWidget
+
+private-phone-apps2 :=  AllshareDMC \
+						AllshareMediaServer \
+						AllSharePlay \
+						BGCleaner \
+						BroadcomMEMSService \
+						CapabilityManagerService \
+						CertInstaller \
+						ChromeBookmarksSyncAdapter \
+						ContextAwareService \
+						DeepSea \
+						DirectShareManager \
+						DrmProvider \
+						DSMForwarding \
+						DSMLawmo \
+						EdmSysScopeService \
+						EdmVpnServices \
+						EnterprisePermissions \
+						Hearingdro \
+						INDI_Server \
+						JobManager \
+						NetworkLocation \
+						NoiseField \
+						OmaCP \
+						OmaDrmPopup \
+						PackageInstaller \
+						PCWClientS \
+						PhaseBeam \
+						PRUI \
+						SecurityProvider \
+						ShaoNv \
+						TwDVFSApp \
+						VpnDialogs \
+						WfdBroker \
+						BluetoothAvrcp \
+						BluetoothMap \
+						BluetoothTest
+#DreyeforSamsung
